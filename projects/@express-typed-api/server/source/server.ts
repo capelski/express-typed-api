356d33a7b6d34ec59db369798323f63fc8310c76
Carles Capellas <capellas.carles@gmail.com>
2022-12-11 16:08:57 +0100

    Rename jsonEndpoint to wrapHandler

--- projects/@express-typed-api/server/source/server.ts
+++ projects/@express-typed-api/server/source/server.ts
@@ -6,7 +6,23 @@ import {
 } from '@express-typed-api/common';
 import express from 'express';
 
-const jsonEndpoint = <T>(
+export const publishApi = <T extends ApiEndpoints>(
+  app: express.Express | express.Router,
+  apiEndpoints: T
+) => {
+  Object.keys(apiEndpoints).forEach((path) => {
+    const availableMethods = apiEndpoints[path];
+    Object.keys(availableMethods)
+      .map((method) => method as EndpointMethod)
+      .forEach((method) => {
+        const endpoint = availableMethods[method]!;
+        const handlers = wrapHandler(endpoint);
+        (<express.Express>app)[method](path, ...handlers);
+      });
+  });
+};
+
+const wrapHandler = <T>(
   endpoint: EndpointHandler<T> | ComposedEndpointHandler<T>
 ): express.RequestHandler[] => {
   const { handler, middleware } =
@@ -21,20 +37,4 @@ const jsonEndpoint = <T>(
   };
 
   return middleware ? middleware(adapter) : [adapter];
-};
-
-export const publishApi = <T extends ApiEndpoints>(
-  app: express.Express | express.Router,
-  apiEndpoints: T
-) => {
-  Object.keys(apiEndpoints).forEach((path) => {
-    const availableMethods = apiEndpoints[path];
-    Object.keys(availableMethods)
-      .map((method) => method as EndpointMethod)
-      .forEach((method) => {
-        const endpoint = availableMethods[method]!;
-        const handlers = jsonEndpoint(endpoint);
-        (<express.Express>app)[method](path, ...handlers);
-      });
-  });
 };
