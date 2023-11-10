ca5bc028d9e82105406b56d0a119698724cbc68d
Carles Capellas <capellas.carles@gmail.com>
2023-11-10 13:38:38 +0100

    Refine publish script

--- publish.sh
+++ publish.sh
@@ -1,20 +1,35 @@
 # Meant for documentation purposes, since the publish step requires 2FA validation
 
-source ./link-destroy.sh
-
 cd projects/@express-typed-api/common
 npm version patch # patch, minor or major
 npm publish
-
-cd ../client 
-npm i -S @express-typed-api/common@latest
-npm version patch # patch, minor or major
-npm publish
-
-cd ../server 
-npm i -S @express-typed-api/common@latest
-npm version patch # patch, minor or major
-npm publish
-
 cd ../../..
-npm i
+
+cd projects/@express-typed-api/client
+npm i -S @express-typed-api/common@latest
+npm version patch # patch, minor or major
+npm publish
+cd ../../..
+
+cd projects/@express-typed-api/server 
+npm i -S @express-typed-api/common@latest
+npm version patch # patch, minor or major
+npm publish
+cd ../../..
+
+cd projects/@sample-express-app/common 
+npm i -S @express-typed-api/common@latest
+cd ../../..
+
+cd projects/@sample-express-app/client 
+npm i -S @express-typed-api/client@latest
+cd ../../..
+
+cd projects/@sample-express-app/server 
+npm i -S @express-typed-api/server@latest
+cd ../../..
+
+npm i # Necessary to adjust package-lock.json after installs
+
+git add .
+git commit -m "version X.Y.Z"
